c80f17c15ca69955ec927ff480023bd2987e69d2
Laura Morales Ariza <lauramoralesariza@192.168.1.7>
2024-10-12 14:19:34 -0500

    wedding folder and deploy gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Alain-y-Laura/",
   plugins: [react()],
-  base: "/vite-react-deploy/"
+  server:{
+    host: true,
+  }
 })
